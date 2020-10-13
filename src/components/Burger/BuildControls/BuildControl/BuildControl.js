2b86e2e937ba6f3640c41e05bcb3a9ada87eb609
urke90 <urosbijelic90@gmail.com>
2020-10-13 11:43:22 +0200

    add,remove ingredient functionality, display total price

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -2,11 +2,24 @@ import React from "react";
 
 import classes from "./BuildControl.module.css";
 
-const buildControl = ({ label }) => (
+const buildControl = ({
+  label,
+  addIngredient,
+  removeIngredient,
+  disabledBtn,
+}) => (
   <div className={classes.BuildControl}>
     <div className={classes.Label}>{label}</div>
-    <button className={classes.Less}>-</button>
-    <button className={classes.More}>+</button>
+    <button
+      disabled={disabledBtn}
+      onClick={removeIngredient}
+      className={classes.Less}
+    >
+      -
+    </button>
+    <button onClick={addIngredient} className={classes.More}>
+      +
+    </button>
   </div>
 );
 
