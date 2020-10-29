c3e87322327666d44b3e23b8560d6b5d9a3e5ef6
urke90 <urosbijelic90@gmail.com>
2020-10-29 13:08:18 +0100

    created contactData component

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import { Route } from "react-router-dom";
+import ContactData from "./ContactData/ContactData";
 
 import CheckoutSummary from "../../components/Order/CheckoutSummary/CheckoutSummary";
 
@@ -12,12 +14,23 @@ class Checkout extends Component {
     },
   };
 
+  componentDidMount() {
+    const { search } = this.props.location;
+    const query = new URLSearchParams(search);
+    const ingredients = {};
+    for (let param of query) {
+      ingredients[param[0]] = +param[1];
+    }
+    this.setState({ ingredients });
+  }
+
   checkoutContinuedHandler = () =>
     this.props.history.replace("/checkout/contact-data");
 
   checkoutCancelledHandler = () => this.props.history.goBack();
 
   render() {
+    const { url } = this.props.match;
     const { ingredients } = this.state;
     return (
       <div>
@@ -26,6 +39,7 @@ class Checkout extends Component {
           checkoutContinued={this.checkoutContinuedHandler}
           ingredients={ingredients}
         />
+        <Route path={`${url}/contact-data`} component={ContactData} />
       </div>
     );
   }
